859a091f4463e52482d020a5a1bf7bb43deb5b83
BluberryPie <blub3rrypie@gmail.com>
2026-08-23 16:45:58 +0900

    add: greedy sampling method

--- src/attention_lab/models/seq2seq.py
+++ src/attention_lab/models/seq2seq.py
@@ -44,3 +44,27 @@ class Seq2Seq(nn.Module):
             all_weights, dim=1
         )  # Shape (batch, target_len-1, source_len)
         return logits_stacked, weights_stacked
+
+    @torch.no_grad()
+    def generate(
+        self, source_ids: torch.Tensor, sos_id: int, max_len: int = 100
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        self.eval()
+        encoder_outputs, decoder_hidden = self.encoder(source_ids)
+        mask = source_ids != self.pad_id
+        prev_token = torch.full(
+            size=(source_ids.shape[0],), fill_value=sos_id, device=source_ids.device
+        )
+        all_tokens: list[torch.Tensor] = []
+        all_weights: list[torch.Tensor] = []
+        for _ in range(max_len):
+            logits, decoder_hidden, weights = self.decoder(
+                prev_token, decoder_hidden, encoder_outputs, mask
+            )
+            prev_token = torch.argmax(input=logits, dim=1)
+            all_tokens.append(prev_token)
+            all_weights.append(weights)
+        self.train()
+        tokens_stacked = torch.stack(all_tokens, dim=1)
+        weights_stacked = torch.stack(all_weights, dim=1)
+        return tokens_stacked, weights_stacked
